2f838fd8ce2f5abc3c9c85068025eb4297f16c79
goodmean <zizigoo12@gmail.com>
2021-11-16 19:40:57 +0900

    게시물 리스트에 검색기능 구현

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -46,14 +46,34 @@ public class App {
 
 				System.out.printf("%d번글이 생성되었습니다.\n", id);
 
-			} else if (command.equals("article list")) {
+			} else if (command.startsWith("article list")) {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
+				
+				String searchKeyword= command.substring("article list".length()).trim();
+				
+				List<Article> forListArticles = articles;
+				
+				if(searchKeyword.length() > 0) {
+					forListArticles = new ArrayList<>();
+					
+					for( Article article : articles ) {
+						if( article.title.contains(searchKeyword) ) {
+							forListArticles.add(article);
+						}
+					}
+					
+					if (articles.size() == 0) {
+						System.out.println("검색 결과가 존재하지 않습니다.");
+						continue;
+					}
+				}
+				
 				System.out.println("번호 | 조회 | 제목");
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
+				for (int i = forListArticles.size() - 1; i >= 0; i--) {
+					Article article = forListArticles.get(i);
 
 					System.out.printf("%4d | %4d | %s\n", article.id, article.hit, article.title);
 				}
@@ -109,8 +129,8 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				int foundIndex = getArticleIndexById(id); 
-						
+				int foundIndex = getArticleIndexById(id);
+
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
@@ -138,14 +158,14 @@ public class App {
 			}
 			i++;
 		}
-		
+
 		return -1;
 	}
 
 	private Article getArticleById(int id) {
 		int index = getArticleIndexById(id);
-		
-		if(index != -1) {
+
+		if (index != -1) {
 			return articles.get(index);
 		}
 		return null;
