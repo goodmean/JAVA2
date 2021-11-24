cd75bd20ac5fa9d37a561451795e91c5c1c1b073
goodmean <zizigoo12@gmail.com>
2021-11-24 20:29:30 +0900

    프로그램 시작 시 테스트 회원 3명 생성

--- src/com/sbs/java/ssg/controller/MemberController.java
+++ src/com/sbs/java/ssg/controller/MemberController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.sbs.java.ssg.dto.Article;
 import com.sbs.java.ssg.dto.Member;
 import com.sbs.java.ssg.util.Util;
 
@@ -17,7 +18,7 @@ public class MemberController extends Controller {
 	public void doAction(String command, String actionMethodName) {
 		this.command = command;
 		this.actionMethodName = actionMethodName;
-
+		
 		switch (actionMethodName) {
 		case "join":
 			doJoin();
@@ -139,6 +140,15 @@ public class MemberController extends Controller {
 		members.add(member);
 
 		System.out.printf("%d번회원이 생성되었습니다.\n", id);
+
+	}
+	
+	public void makeTestData() {
+		System.out.println("테스트를 위한 회원 데이터를 생성합니다.");
+
+		members.add(new Member(1, Util.getNowDateStr(), "admin", "admin", "관리자"));
+		members.add(new Member(2, Util.getNowDateStr(), "user1", "user1", "유저1"));
+		members.add(new Member(3, Util.getNowDateStr(), "user2", "user2", "유저2"));
 
 	}
 
